0eda942b2db70fcf08748ebe45a18cbd46cb2ab4
AnasChaairi <52534492+AnasChaairi@users.noreply.github.com>
2023-05-21 00:14:32 +0100

    test.py

--- test.py
+++ test.py
@@ -18,7 +18,6 @@ with sr.Microphone() as source:
     print("Talk")
     audio_text = r.listen(source)
     print("Time over, thanks")
-recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
     
     try:
         # using google speech recognition
